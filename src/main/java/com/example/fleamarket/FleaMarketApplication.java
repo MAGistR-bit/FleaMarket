376162e3d958dea66fce1fb4d90cdc2e200817b8
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2023-07-29 20:12:15 +0300

    Initial commit

---
+++ src/main/java/com/example/fleamarket/FleaMarketApplication.java
@@ -0,0 +1,14 @@
+package com.example.fleamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class FleaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fleamarket/FleaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fleamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
