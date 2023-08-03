72921d884a22880dcf1bf94f94ec39b00d027852
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2023-08-03 17:16:48 +0300

    Update service (UserService)

--- src/main/java/com/example/fleamarket/services/UserService.java
+++ src/main/java/com/example/fleamarket/services/UserService.java
@@ -8,6 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * Логика регистрации пользователя
  */
@@ -45,5 +51,52 @@ public class UserService {
         // Сохранить пользователя
         userRepository.save(user);
         return true;
+    }
+
+    /**
+     * Получает пользователей, зарегистрированных в системе
+     *
+     * @return список пользователей
+     */
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * Блокирует пользователя (запрещает вход в систему)
+     *
+     * @param id идентификатор пользователя
+     */
+    public void banUser(Long id) {
+        // Получить пользователя по идентификатору
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            if (user.isActive()) {
+                user.setActive(false);
+                log.info("Ban user with id = {}; email: {}",
+                        user.getId(),
+                        user.getEmail());
+            } else {
+                user.setActive(true);
+                log.info("Unban user with id = {}; email: {}",
+                        user.getId(),
+                        user.getEmail());
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void changeUserRoles(User user, Map<String, String> form) {
+        // Преобразовать роли в строковый вид
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
     }
 }
